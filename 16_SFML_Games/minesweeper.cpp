7289d3771685f1f2418b796323408b15da537d71
(Student  C00313099) Mykyta Kozlovskyi <C00313099@setu.ie>
2025-11-12 14:54:30 +0000

    made comments for each line of code (didn't refactor anything yet)

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -8,22 +8,34 @@ int minesweeper()
 
     RenderWindow app(VideoMode(400, 400), "Minesweeper!");
 
+    //probably width
     int w=32;
+
+    
     int grid[12][12];
     int sgrid[12][12]; //for showing
 
+
+    //texture 
     Texture t;
     t.loadFromFile("images/minesweeper/tiles.jpg");
+    
+    
     Sprite s(t);
 
     for (int i=1;i<=10;i++)
      for (int j=1;j<=10;j++)
       {
+        // every square isn't open 
         sgrid[i][j]=10;
+
+        //putting mines, default chanse that there is mine = 25%
         if (rand()%5==0)  grid[i][j]=9;
+        //else 75% is for empty square
         else grid[i][j]=0;
       }
 
+    //this for loop is used for defining how many mines near this square
     for (int i=1;i<=10;i++)
      for (int j=1;j<=10;j++)
       {
@@ -40,23 +52,33 @@ int minesweeper()
         grid[i][j]=n;
       }
 
+    //while player is playing 
     while (app.isOpen())
     {
+        // mouse position
         Vector2i pos = Mouse::getPosition(app);
+        
+        //current position of mouse
         int x = pos.x/w;
         int y = pos.y/w;
 
         Event e;
         while (app.pollEvent(e))
-        {
+        {   
+            //if player decide to close the game
             if (e.type == Event::Closed)
                 app.close();
 
+
             if (e.type == Event::MouseButtonPressed)
+                //when player left click on square depends on what does the square actually contain it's changing on mine/empty square
               if (e.key.code == Mouse::Left) sgrid[x][y]=grid[x][y];
+                //if player right click on square the flag image appears on this square
               else if (e.key.code == Mouse::Right) sgrid[x][y]=11;
         }
 
+
+        //Background color
         app.clear(Color::White);
 
         for (int i=1;i<=10;i++)
